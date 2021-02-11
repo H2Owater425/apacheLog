18fab5f0dd72f4150aee246da1f9c01f5aa586a0
h2owater425 <h2o@h2owr.xyz>
2021-02-11 20:42:47 +0900

    feat: Update error messages to fit in form

--- main.py
+++ main.py
@@ -15,17 +15,19 @@ class ApacheLog:
 		- export: _PathLike
 		'''
 
+		errorMessage = '"{}" is unsupported {} for "{}"'
+
 		if(str(type(timeZone)) != '<class \'str\'>'):
-			raise TypeError('unsupported type for timeZone('+str(type(timeZone)).replace('<class \'', '').replace('\'>', '')+')')
+			raise TypeError(errorMessage.format(str(type(timeZone)).replace('<class \'', '').replace('\'>', '')), 'type', 'timeZone')
 
 		elif(str(type(logLevel)) != '<class \'str\'>' and str(type(logLevel)) != '<class \'int\'>'):
-			raise TypeError('unsupported type for logLevel('+str(type(logLevel)).replace('<class \'', '').replace('\'>', '')+')')
+			raise TypeError(errorMessage.format(str(type(logLevel)).replace('<class \'', '').replace('\'>', '')), 'type', 'logLevel')
 
 		elif(str(type(message)) != '<class \'str\'>'):
-			raise TypeError('unsupported type for message('+str(type(message)).replace('<class \'', '').replace('\'>', '')+')')
+			raise TypeError(errorMessage.format(str(type(message)).replace('<class \'', '').replace('\'>', '')), 'type', 'message')
 
 		elif(str(type(export)) != '<class \'str\'>' and str(type(export)) != '<class \'NoneType\'>'):
-			raise TypeError('unsupported type for export('+str(type(export)).replace('<class \'', '').replace('\'>', '')+')')
+			raise TypeError(errorMessage.format(str(type(export)).replace('<class \'', '').replace('\'>', '')), 'type', 'export')
 
 		# debug: 0, info: 1, notice: 2, warn: 3, alert: 4, error: 5, crit: 6, emerg: 7
 		logLevelNameList = ['debug', 'info', 'notice', 'warn', 'alert', 'error', 'crit', 'emerg']
@@ -37,18 +39,18 @@ class ApacheLog:
 			export = export.replace('\\', '/')
 
 		if(re.match(re.compile('^[+-](?:2[0-3]|[01][0-9]):[0-5][0-9]$'), timeZone) == None):
-			raise ValueError(f'unsupported string form for timeZone("{timeZone}")')
+			raise ValueError(errorMessage.format(timeZone, 'string', 'timeZone'))
 
 		if(str(type(logLevel)) == '<class \'str\'>'):
 			if(not logLevel in logLevelNameList):
-				raise ValueError(f'unsupported string form for logLevel("{logLevel}")')
+				raise ValueError(errorMessage.format(logLevel, 'string', 'logLevel'))
 
 		elif(str(type(logLevel)) == '<class \'int\'>'):
 			if(logLevel < 8 and logLevel > -1):
 				logLevel = logLevelNameList[logLevel]
 
 			else:
-				raise ValueError(f'unsupported integer for logLevel("{logLevel}")')
+				raise ValueError(errorMessage.format(logLevel, 'integer', 'logLevel'))
 
 		if(export != None):
 			if(len(export.split('/')[:-1]) != 0 and not os.path.exists('/'.join(export.split('/')[:-1]))):
@@ -58,7 +60,7 @@ class ApacheLog:
 				logFile = open(export, 'a', encoding='utf8')
 
 			except PermissionError:
-				raise PermissionError(f'wrong or have no permission for log file("{export}")')
+				raise PermissionError(f'Wrong or have no permission for the file "{export}"')
 
 			currentTime = datetime.now(timezone("UTC")).strftime('%d/%b/%Y:%H:%M:%S')
 
